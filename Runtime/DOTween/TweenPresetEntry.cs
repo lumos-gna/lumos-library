b3c63ea9e9c9cc353facf2c87c7b36a1e4098292
lumos <lumos.wge@gmail.com>
2025-11-15 19:57:05 +0900

    refactor tween preset
    각 preset 별, preset animator 하나로 사용 통일

---
+++ Runtime/DOTween/TweenPresetEntry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using LumosLib;
+using TriInspector;
+using UnityEngine;
+
+namespace DOTween
+{
+    [Serializable]
+    public class TweenPresetEntry<T> where T : BaseTweenPreset
+    {
+        [Group("Key"), HideLabel]
+        [SerializeField] private string _key;
+        
+        [Group("Preset"), HideLabel]
+        [SerializeField] private T _preset;
+
+        public T GetPreset()
+        {
+            return _preset;
+        }
+
+        public string GetKey()
+        {
+            return _key;
+        }
+    }
+}
---
+++ Runtime/DOTween/TweenPresets/FadeTweenPreset.cs
@@ -0,0 +1,39 @@
+﻿using DG.Tweening;
+using TriInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LumosLib
+{
+    [CreateAssetMenu(fileName = "FadeTweenPreset", menuName = "[ ✨Lumos Lib ]/Scriptable Object/Tween Preset/Fade", order = int.MinValue)]
+    public class FadeTweenPreset : BaseTweenPreset
+    {
+  
+        [Title("Fade")] 
+        [SerializeField] private float _fade;
+        
+        [SerializeField] private bool _useInitFade;
+        [SerializeField, ShowIf("_useInitFade")] private float _initFade;
+        
+
+        #region >--------------------------------------------------- GET
+
+
+        public float GetFade()
+        {
+            return _fade;
+        }
+
+        public bool GetUseInitFade()
+        {
+            return _useInitFade;
+        }
+
+        public float GetInitFade()
+        {
+            return _initFade;
+        }
+
+        #endregion
+    }
+}
