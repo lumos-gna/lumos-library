5020d089ac1f362b447e183954838b41a32e3c38
lumos <lumos.wge@gmail.com>
2025-11-20 21:05:43 +0900

    add eventbus

---
+++ Runtime/Core/Event/EventBus.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LumosLib
+{
+    public class EventBus : IEventBus
+    {
+        private readonly Dictionary<Type, List<Delegate>> _table = new ();
+
+        public void Subscribe<T>(Action<T> listener)
+        {
+            var type = typeof(T);
+
+            if (!_table.TryGetValue(type, out var list))
+            {
+                list = new List<Delegate>();
+                _table[type] = list;
+            }
+
+            list.Add(listener);
+        }
+
+        
+        public void Unsubscribe<T>(Action<T> listener)
+        {
+            var type = typeof(T);
+
+            if (_table.TryGetValue(type, out var list))
+            {
+                list.Remove(listener);
+
+                if (list.Count == 0)
+                    _table.Remove(type);
+            }
+        }
+
+        
+        public void Publish<T>(T evt)
+        {
+            var type = typeof(T);
+
+            if (_table.TryGetValue(type, out var list))
+            {
+                var temp = list.ToArray();
+
+                foreach (var del in temp)
+                {
+                    (del as Action<T>)?.Invoke(evt);
+                }
+            }
+        }
+    }
+}
---
+++ Runtime/Core/Event/IEventBus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LumosLib
+{
+    public interface IEventBus
+    {
+        void Subscribe<T>(Action<T> listener);
+        void Unsubscribe<T>(Action<T> listener);
+        void Publish<T>(T evt);
+    }
+}
